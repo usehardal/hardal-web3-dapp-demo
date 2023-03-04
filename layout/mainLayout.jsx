523941ffec92eabdabd5b96b6126332f03809823
raufsamestone <berkaydemirbas@gmail.com>
2023-03-04 19:43:42 +0300

    updated

--- layout/mainLayout.jsx
+++ layout/mainLayout.jsx
@@ -1,10 +1,19 @@
 import Navbar from "../components/navigation/navbar";
+import Head from "next/head";
 
 export default function MainLayout({ children }) {
-	return (
-		<div>
-            <Navbar />
-            {children}
-		</div>
-	);
+  return (
+    <div>
+      <Head>
+        {/* <script
+          async
+          defer
+          data-website-id="0623848d-93db-4e7d-ae28-b82ed7475b95"
+          src="https://app.usehardal.com/hardal.js"
+        ></script> */}
+      </Head>
+      <Navbar />
+      {children}
+    </div>
+  );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,16 @@
 import styles from "../styles/Home.module.css";
 import InstructionsComponent from "../components/InstructionsComponent";
+import { useAccount } from "wagmi";
+import { useEffect } from "react";
+import { loadMyLib } from "../../hardal/dist/index";
 
 export default function Home() {
+  const { address, isConnecting, isDisconnected } = useAccount();
+
+  console.log(address, isConnecting, isDisconnected);
+
+  loadMyLib();
+
   return (
     <div>
       <main className={styles.main}>
